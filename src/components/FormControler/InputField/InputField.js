2f8d4fbdfefeb13900effebf628d25b0f02b41ea
DucAnh <ducanh.le1908@gmail.com>
2022-09-28 16:28:21 +0700

    ui register success

--- src/components/FormControler/InputField/InputField.js
+++ src/components/FormControler/InputField/InputField.js
@@ -1,31 +1,45 @@
-import { FormHelperText, TextField } from '@mui/material';
-import React from 'react'
-import {Controller} from 'react-hook-form'
-const InputField = (props) => {
-    const {form, name, label, disabled} = props;
-    const {formState:{errors}} = form;
-    const hasError = errors[name];
-  return (
-  
-      <>
-      <Controller
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Controller } from 'react-hook-form';
+import { TextField, FormHelperText} from '@mui/material';
+
+InputField.propTypes = {
+    form : PropTypes.object.isRequired,
+    name : PropTypes.string.isRequired,
+
+    label: PropTypes.string,
+    disabled : PropTypes.bool
+};
+
+function InputField(props) {
+
+    const { form, name, label, disabled } = props;
+    const {formState : {errors}} = form;
+    const hasError = errors[name]
+
+    return (
+        <>  
+        <Controller 
         name={name}
         control={form.control}
-        render = {({field}) => {
+        render = {({ field})=> (
             <TextField
-            {...field}
-            fullWidth
-            label={label}
-            disabled={disabled}
-            />
-        }
-        }
-        />
-        <FormHelperText errors ={!!hasError}>
-        {errors[name]?.message}
-        </FormHelperText>
-      </>
-  )
+                {...field}
+                sx ={{ width :"30%", m:1}}
+                label={label}
+                disabled={disabled}
+                errors= {!!hasError}
+            />         
+        )}
+        />     
+            <FormHelperText error={!!hasError}>
+                {errors[name]?.message}
+            </FormHelperText>
+        </>
+      
+        
+    );
 }
 
-export default InputField
+export default InputField;
