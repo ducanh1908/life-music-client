932802b392983a05e93479b69032e2aeb2cec6d2
DucAnh <ducanh.le1908@gmail.com>
2022-09-29 16:18:52 +0700

    fix merge

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import Body from './../../components/Body/Body';
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 import Navbar from './../../components/Navbar/Navbar';
 
