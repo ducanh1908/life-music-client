a8e362d89ad73bcdc5340e4d5b048c2c2479e9bc
DucAnh <ducanh.le1908@gmail.com>
2022-09-30 00:28:10 +0700

    login success

--- src/redux/userSlice/userSlice.js
+++ src/redux/userSlice/userSlice.js
@@ -10,9 +10,17 @@ export const register = createAsyncThunk(
         localStorage.setItem('user', JSON.stringify(data.user))
         console.log(data.user);
         return data.user;
-    } 
-    
-  )
+    });
+
+    export const login = createAsyncThunk(
+        'user/login',
+        async (payload) => {
+            const data = await userApi.login(payload);
+            localStorage.setItem('access_token',data.access_token)
+            localStorage.setItem('user', JSON.stringify(data.user))
+            console.log(data.user);
+            return data.user;
+        });
 const initialState = {
     user:{},
     loading: false,
@@ -26,6 +34,10 @@ const userSlice = createSlice({
     extraReducers : {
         [register.fulfilled]: (state, action) => {
             state.user = action.payload
+        },
+        [login.fulfilled]: (state, action) => {
+            state.user = action.payload
+            
         }
     }
 })
