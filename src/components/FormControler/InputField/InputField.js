4f97e03c3299117ccc85fb0b72649f34258dfcae
PhgX <phuongbm211@gmail.com>
2022-09-30 08:21:10 +0700

    pull FE

--- src/components/FormControler/InputField/InputField.js
+++ src/components/FormControler/InputField/InputField.js
@@ -1,45 +1,44 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import { Controller } from 'react-hook-form';
-import { TextField, FormHelperText} from '@mui/material';
+import { Controller } from "react-hook-form";
+import { TextField, FormHelperText } from "@mui/material";
 
 InputField.propTypes = {
-    form : PropTypes.object.isRequired,
-    name : PropTypes.string.isRequired,
+  form: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
 
-    label: PropTypes.string,
-    disabled : PropTypes.bool
+  label: PropTypes.string,
+  disabled: PropTypes.bool,
 };
 
 function InputField(props) {
+  const { form, name, label, disabled } = props;
+  const {
+    formState: { errors },
+  } = form;
+  const hasError = errors[name];
 
-    const { form, name, label, disabled } = props;
-    const {formState : {errors}} = form;
-    const hasError = errors[name]
-
-    return (
-        <>  
-        <Controller 
+  return (
+    <>
+      <Controller
         name={name}
         control={form.control}
-        render = {({ field})=> (
-            <TextField
-                {...field}
-                sx ={{ width :"30%", m:1}}
-                label={label}
-                disabled={disabled}
-                errors= {!!hasError}
-            />         
+        render={({ field }) => (
+          <TextField
+            {...field}
+            sx={{ width: "30%", m: 1 }}
+            label={label}
+            disabled={disabled}
+            errors={!!hasError}
+          />
         )}
-        />     
-            <FormHelperText error={!!hasError}>
-                {errors[name]?.message}
-            </FormHelperText>
-        </>
-      
-        
-    );
+      />
+      <FormHelperText error={!!hasError}>
+        {errors[name]?.message}
+      </FormHelperText>
+    </>
+  );
 }
 
 export default InputField;
