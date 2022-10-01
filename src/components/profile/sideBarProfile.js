521b31c8f6b13473d78585132c658b7b2e0cd99d
quyendch11 <quyendch11@gmail.com>
2022-10-01 16:11:39 +0700

    fix update profile

--- src/components/profile/sideBarProfile.js
+++ src/components/profile/sideBarProfile.js
@@ -40,6 +40,16 @@ const Title = styled.div`
   font-size: 20px;
   color: black;
 `
+const InforAvatar=styled.div`
+  width: 150px;
+  height: 150px;
+  overflow: hidden;
+  border-radius: 50%;
+  position: relative;
+  margin: 15px auto;
+  border: 1px solid #ddd;
+  cursor: pointer;
+`
 const SideBarProfile = () => {
     const user = useSelector(state=> state.user.user)
     const [avatar, setAvatar] = useState('')
@@ -47,17 +57,17 @@ const SideBarProfile = () => {
         <Container2>
             <Logo>
                 <form>
-                    <Avatar alt="Cindy Baker" src={user.profileImage} sx={{width:150 ,height:150}}/>
-                    {/*<div className="info_avatar">*/}
-                    {/*    <img src={avatar ? URL.createObjectURL(avatar) :user.avatar}*/}
-                    {/*         alt="avatar" />*/}
-                    {/*    <span>*/}
-                    {/*    <i className="fas fa-camera"/>*/}
-                    {/*    <p>Change</p>*/}
-                    {/*    <input type="file" name="file" id="file_up"*/}
-                    {/*           accept="image/*" />*/}
-                    {/*</span>*/}
-                    {/*</div>*/}
+                    {/*<Avatar alt="Cindy Baker" src={user.profileImage} sx={{width:150 ,height:150}}/>*/}
+                    <InforAvatar>
+                        <img src={avatar ? URL.createObjectURL(avatar) :user.avatar}
+                             alt="avatar" />
+                        <span>
+                        <i className="fas fa-camera"/>
+                        <p>Change</p>
+                        <input type="file" name="file" id="file_up"
+                               accept="image/*" />
+                    </span>
+                    </InforAvatar>
                 </form>
 
             </Logo>
