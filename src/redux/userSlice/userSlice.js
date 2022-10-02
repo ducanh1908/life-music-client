e860bee4a77fead825ce9a4e6809188b473db927
PhgX <phuongbm211@gmail.com>
2022-10-02 23:54:33 +0700

    merge dev 12h

--- src/redux/userSlice/userSlice.js
+++ src/redux/userSlice/userSlice.js
@@ -10,6 +10,7 @@ const initialState = {
 
 export const register = createAsyncThunk("user/register", async (payload) => {
   const data = await userApi.register(payload);
+  if(data.user)
   localStorage.setItem("user", JSON.stringify(data.user));
   console.log(data.user);
   return data.user;
