cea8d67b19a9319f66236c530cf153dc63b5ecf9
DucAnh <ducanh.le1908@gmail.com>
2022-10-07 13:35:13 +0700

    fix audio next pre

--- src/components/HomeFooter/DetailSong.js
+++ src/components/HomeFooter/DetailSong.js
@@ -1,14 +1,39 @@
 import React from 'react'
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
+import  styled from 'styled-components';
 
-const DetailSong = () => {
+const Container = styled.div `
+display: flex;
+/* align-items: center; */
+justify-content: flex-start;
+padding: 20px;
+`
+const Song = styled.div`
+`
+const SongImage = styled.img`
+width: 60px;
+height: 60px;
+`
+const SongName = styled.span`
+margin: 20px;
+`
+const SongSinger = styled.div`
+`
+const DetailSong = ({song, trackIndex}) => {
+  let {
+    name = "",
+    file = "",
+    image = "",
+  } = trackIndex !== -1 ? song[trackIndex] : {};
 
-  const dispatch = useDispatch();
-    const song = useSelector(state=> state.currentSong.currentSong)
-    
+  console.log({image})
   return (
-    <div>DetailSong : {song.name} </div>
+     
+    <Container>
+      <SongImage src={image} />
+      <SongName >{name.slice(0, 15)} </SongName> 
+      </Container>
   )
 }
 
