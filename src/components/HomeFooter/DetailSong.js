eba40aee95b76c4e47d9557a3705b84f3f52db16
quyendch11 <quyendch11@gmail.com>
2022-10-09 17:44:23 +0700

    play music in playlistadmin

--- src/components/HomeFooter/DetailSong.js
+++ src/components/HomeFooter/DetailSong.js
@@ -32,7 +32,7 @@ const DetailSong = ({song, trackIndex}) => {
   return (
      
     <Container>
-      <SongImage src={image} />
+      <SongImage src={image ? image :"https://images.unsplash.com/photo-1458560871784-56d23406c091?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"} />
       <SongName >{name.slice(0, 15)} </SongName> 
       </Container>
   )
